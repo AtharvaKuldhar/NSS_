8f9f0b96c550489adf9e553158897bee2be1870c
atharvakuldhar <atharvakuldhar@gmail.com>
2025-08-13 21:08:09 +0530

    fixed homepage rediect

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,19 @@
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { LoaderPinwheel } from "lucide-react";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import { LoaderPinwheel } from "lucide-react";
+// export default function Home() {
+//   const router = useRouter()
+//   useEffect(()=>{
+//     router.push("/auth/login")
+//   })
+//   return (
+//     <div className="w-full h-full flex justify-center items-center"><LoaderPinwheel/></div>
+//   );  
+// }
+
+// src/app/page.tsx
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  const router = useRouter()
-  useEffect(()=>{
-    router.push("/auth/login")
-  })
-  return (
-    <div className="w-full h-full flex justify-center items-center"><LoaderPinwheel/></div>
-  );
+  redirect("/auth/login");
 }
